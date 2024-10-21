13a504786172eb2db88ebda0ac132a50c0e8a4fe
Tatu Lehtola <tatu.lehtola1996@gmail.com>
2024-10-21 19:46:17 +0300

    Updates

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -10,6 +10,7 @@ function showSlides() {
     setTimeout(showSlides, 6000); // Change image every 3 seconds
 }
 
+
 function changeImageBasedOnH2() {
     const h2Element = document.querySelector('h2');
     const imageFrame = document.getElementById('imageFrame');
@@ -72,10 +73,9 @@ function changeImageBasedOnH2() {
         updateImage();
         setInterval(updateImage, 6000);
     }
-    // Start the image rotation
+
     startImageRotation();
 
-    // Watch for changes in the h2 element
     const observer = new MutationObserver(() => {
         currentIndex = 0;
         startImageRotation();
@@ -84,7 +84,6 @@ function changeImageBasedOnH2() {
     observer.observe(h2Element, { childList: true, characterData: true, subtree: true });
 }
 
-// Call the function when the page loads
 window.addEventListener('load', changeImageBasedOnH2);
 
 function updateH2FromDropdown() {
@@ -102,3 +101,46 @@ function updateH2FromDropdown() {
 // Call the function when the page loads
 window.addEventListener('load', updateH2FromDropdown);
 
+function updateMapBasedOnDropdown() {
+    const dropdown = document.getElementById('locationDropdown');
+    const mapContainer = document.querySelector('.map-container');
+
+    if (dropdown && mapContainer) {
+        dropdown.addEventListener('change', function() {
+            const selectedLocation = this.value;
+            let mapSrc = '';
+
+            switch (selectedLocation) {
+                case 'Kaitsajoki':
+                    mapSrc = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1653.9201631046915!2d28.45583!3d69.44583!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x45cb49c8a2af65b9%3A0x9b46049f3f7c81c4!2sKaitsajoki!5e0!3m2!1sen!2sus!4v1683737000000!5m2!1sen!2sus';
+                    break;
+                case 'Sandfjordelva':
+                    mapSrc = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1653.9201631046915!2d28.15583!3d69.64583!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x45cb49c8a2af65b9%3A0x9b46049f3f7c81c4!2sSandfjordelva!5e0!3m2!1sen!2sus!4v1683737000000!5m2!1sen!2sus';
+                    break;
+                case 'Vuollajohka':
+                    mapSrc = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1653.9201631046915!2d28.25583!3d69.54583!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x45cb49c8a2af65b9%3A0x9b46049f3f7c81c4!2sVuolajohka!5e0!3m2!1sen!2sus!4v1683737000000!5m2!1sen!2sus';
+                    break;
+                case 'Valtijoki':
+                    mapSrc = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1653.9201631046915!2d28.35583!3d69.34583!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x45cb49c8a2af65b9%3A0x9b46049f3f7c81c4!2sValtijoki!5e0!3m2!1sen!2sus!4v1683737000000!5m2!1sen!2sus';
+                    break;
+                default:
+                    mapSrc = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1653.9201631046915!2d28.45583!3d69.44583!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x45cb49c8a2af65b9%3A0x9b46049f3f7c81c4!2sKaitsajoki!5e0!3m2!1sen!2sus!4v1683737000000!5m2!1sen!2sus';
+            }
+
+            const iframe = mapContainer.querySelector('iframe');
+            if (iframe) {
+                iframe.src = mapSrc;
+            } else {
+                const newIframe = document.createElement('iframe');
+                newIframe.src = mapSrc;
+                newIframe.allowFullscreen = true;
+                newIframe.loading = 'lazy';
+                newIframe.referrerPolicy = 'no-referrer-when-downgrade';
+                mapContainer.appendChild(newIframe);
+            }
+        });
+    }
+}
+
+// Call the function when the page loads
+window.addEventListener('load', updateMapBasedOnDropdown);
